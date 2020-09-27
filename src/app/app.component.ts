c1864d4d186a787b4ed946c1c28a32eb67443228
Marcin <grafika.marcinkazmierczak@gmail.com>
2020-09-27 09:35:40 +0200

    Add basic elements of forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forms';
+
+  user: {firstName: String; password: String } = {
+    firstName: '',
+    password: '',
+  };
+
+  onSubmit(): void {
+
+    console.log(this.user);
+  }
 }
